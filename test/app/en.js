048e837a9cd7b65d7069d5b4681bc56a38558ec1
Robert Sköld <robert@publicclass.se>
2011-11-16 11:17:50 +0100

    Moved the test app to test/app.

---
+++ test/app/en.js
@@ -0,0 +1,4 @@
+module.exports = {
+  i: { can: { haz: "localization" } },
+  embedded: "With some %{i.can.haz} from the locale file (which has been passed as a context)."
+}
---
+++ test/app/sv.js
@@ -0,0 +1,4 @@
+module.exports = {
+  i: { can: { haz: "lokalisering" } },
+  embedded: "Med lite %{i.can.haz} från språkfilen."
+}
