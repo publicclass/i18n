ab85a945f31a263917cd302932a89891d069e545
Robert Sköld <robert@publicclass.se>
2011-11-16 11:18:05 +0100

    Remove old test app.

--- test/en.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  i: { can: { haz: "localization" } },
-  embedded: "With some <%= i.can.haz %> from the locale file.",
-  script: "Print a number: <%= 12*5 %> = 60?!"
-}
--- test/sv.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  i: { can: { haz: "lokalisering" } },
-  embedded: "Med lite <%= i.can.haz %> från språkfilen.",
-  script: "Skriv ut ett nummer: <%= 12*5 %> = 60?!"
-}
